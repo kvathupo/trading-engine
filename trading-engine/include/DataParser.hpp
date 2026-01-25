41b5a9fba00a02934de475faea06f5dc0ab9e6de
kvathupo <kalinda619@gmail.com>
2026-01-24 22:00:18 -0800

    add is_data_good

--- trading-engine/include/DataParser.hpp
+++ trading-engine/include/DataParser.hpp
@@ -12,20 +12,20 @@ namespace te {
 struct DataParser {
     DataParser() = default;
     virtual bool init(InitializationConfig cfg) = 0;
+    virtual bool is_data_good() = 0;
     virtual ~DataParser() = default;
 
+    virtual Exchange get_exchange() = 0;
     // TODO(optim): return ref to const string
-    virtual std::string get_exchange() = 0;
     virtual std::string get_ticker() = 0;
 
     virtual std::optional<float> get_newest_price() = 0;
-    virtual std::optional<float> get_newest_time() = 0;
-    virtual std::optional<float> get_tick_duration() = 0;
+    // In epoch time
+    virtual std::optional<std::size_t> get_newest_time() = 0;
+    virtual std::optional<std::size_t> get_tick_duration() = 0;
     virtual std::optional<float> get_transaction_fee() = 0;
     virtual std::optional<std::vector<float>> get_order_book(OrderBookSide side,
         std::uint_fast8_t depth) = 0;
 };
-
-
 
 }   // end namespace te
